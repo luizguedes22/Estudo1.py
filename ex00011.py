421d001685c57c8bbefcb5d2f405400fecadc1f3
Luiz Guedes <luizguedes2225@gmail.com>
2025-11-10 20:09:24 -0300

    Update ex00011.py

---
+++ ex00011.py
@@ -0,0 +1,6 @@
+l=float(input('digite a largura de parede:'))
+a=float(input('digite a altura:'))
+m=l*a
+print(f'sua parede tem a dimensão de {a}x{l} e sua àrea é de {m}m²')
+tinta= a/2
+print(f'para pintar essa parede você irá precisar de {tinta} litros de tinta')
